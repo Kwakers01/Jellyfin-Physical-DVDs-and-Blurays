d35de5d694280dc320f05df7b36b6349c862f625
Kwakers01 <43093502+Kwakers01@users.noreply.github.com>
2026-05-31 06:24:12 +0100

    Add files via upload
    Changed scripts to use the ~/.local directory instead of global, thus not requiring root access.

---
+++ Overlay DVD and Bluray Icons for Jellyfin/bash files/bluray-blue-tm-dc.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+counter=1
+
+while [ "$counter" -le "$#" ]; do
+
+    f="${!counter}"
+
+    [ -f "$f" ] || continue  # Skip if no files match
+
+#Backup original file
+FILE="$f-orig"
+
+if [ ! -f "$FILE" ]; then
+  cp "$f" "$f-orig"  
+fi
+
+#resize file
+convert "$f-orig" -resize 1000x1500! "$f"
+
+#overlay icon
+composite -gravity NorthWest -geometry 165x165+30+30 "~/.local/share/dvd-apps/dvd-bluray-icons/icons/bluray-blue-tm.png" "$f" "$f"
+   
+((counter++))
+done
